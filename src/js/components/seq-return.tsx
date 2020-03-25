7ac36a5408f24dd33fa4c7f4fb8ecdf879fbd7c7
Jan Martinek <honza.martinek@gmail.com>
2020-03-25 18:19:10 +0100

    SeqReturn rewrite

--- src/js/components/seq-return.tsx
+++ src/js/components/seq-return.tsx
@@ -16,6 +16,13 @@ export enum SeqReturnStatus {
   Disabled = 2,
 }
 
+interface IButton {
+  link?: string;
+  click?: (e: React.MouseEvent) => void;
+  primary?: boolean;
+  text: string;
+}
+
 interface IProps extends WithTranslation {
   targetIdea: number | null;
   targetChapter: IDocument | null;
@@ -35,12 +42,13 @@ class SeqReturn extends React.Component<IProps, IState> {
     super(props);
 
     this.state = {
-      collapsed: props.docRole !== DocRole.Index,
+      collapsed: docInfo.role !== DocRole.Index,
     };
   }
 
   resetPosition = (e: React.MouseEvent) => {
     e.preventDefault();
+
     this.props.setPosition(true);
   };
 
@@ -48,75 +56,103 @@ class SeqReturn extends React.Component<IProps, IState> {
     if (this.props.targetIdea) highlightIdea(this.props.targetIdea);
   };
 
-  getOpenFirstChapterContent = () => {
-    return (
-      <>
-        <p>{this.props.t('intro')}</p>
-        <div className="seq-buttons">
-          <a href={this.props.startLink}>
-            <b>{this.props.t('start')}</b>
-          </a>
-        </div>
-      </>
-    );
+  returnToPosition = () => {
+    this.setState({ ...this.state, collapsed: true });
+
+    const sameChapter =
+      this.props.targetChapter && this.props.targetChapter.order === docInfo.order;
+    sameChapter ? this.highlightPosition : null;
   };
 
-  getReturnToChapterContent = () => {
-    if (this.props.targetChapter === null) return null;
+  renderFirstOpen = () => {
+    return this.renderWrapper(null, [
+      {
+        link: this.props.startLink,
+        primary: false,
+        text: `${this.props.t('start')} ➡️`,
+      },
+    ]);
+  };
 
+  renderReturnFromTitle = () => {
     const chapter = this.props.targetChapter;
     const idea = this.props.targetIdea;
+    if (chapter === null || idea === null) return null;
+
     const link = `./${chapter.file}#idea${idea}`;
-    const sameChapter = chapter.order !== docInfo.order;
-    const title = chapter.title;
 
-    const readingPosition =
-      this.props.docRole !== DocRole.Chapter || !sameChapter ? (
-        <p>
-          <Trans i18nKey="navigation:seqReturnAnotherChapter">
-            You read up to <a href={link}>sentence #{{ idea }}</a> in chapter <b>{{ title }}</b>.
-          </Trans>
-        </p>
-      ) : (
-        <p>
-          <Trans i18nKey="navigation:seqReturnThisChapter">
-            You read up to{' '}
-            <a href={link} onClick={this.highlightPosition}>
-              sentence #{{ idea }}
-            </a>{' '}
-            in this chapter.
-          </Trans>
-        </p>
-      );
-
-    return (
-      (this.props.sequential === Sequential.No || this.props.docRole !== DocRole.Chapter) && (
-        <>
-          {readingPosition}
-          <div className="seq-buttons">
-            {this.props.docRole === DocRole.Chapter && (
-              <a href="#" onClick={this.resetPosition}>
-                👇 {this.props.t('continue')}
-              </a>
-            )}
-            <a
-              href={link}
-              onClick={() => {
-                this.setState({ ...this.state, collapsed: true });
-                sameChapter ? this.highlightPosition : null;
-              }}
-            >
-              <b>
-                {this.props.docRole === DocRole.Chapter
-                  ? `🔙 ${this.props.t('return')}`
-                  : this.props.t('continueReading')}
-              </b>
-            </a>
-          </div>
-        </>
-      )
-    );
+    return this.renderWrapper(this.posInAnotherChapter(link, idea, chapter.title), [
+      {
+        link,
+        click: this.returnToPosition,
+        primary: true,
+        text: this.props.t('continueReading'),
+      },
+    ]);
   };
+
+  renderReturnFromOther = () => {
+    const chapter = this.props.targetChapter;
+    const idea = this.props.targetIdea;
+    if (chapter === null || idea === null) return null;
+    if (this.state.collapsed && chapter.order !== null)
+      return this.renderWrapper(`🔙 ${chapter.order + 1}.${idea}`);
+
+    const link = `./${chapter.file}#idea${idea}`;
+
+    return this.renderWrapper(this.posInAnotherChapter(link, idea, chapter.title), [
+      {
+        link,
+        click: this.returnToPosition,
+        primary: true,
+        text: `🔙 ${this.props.t('return')}`,
+      },
+    ]);
+  };
+
+  renderReturnFromChapter = () => {
+    const chapter = this.props.targetChapter;
+    const idea = this.props.targetIdea;
+    if (chapter === null || idea === null) return null;
+    if (this.state.collapsed && chapter.order !== null)
+      return this.renderWrapper(`🔙 ${chapter.order + 1}.${idea}`);
+
+    const link = `./${chapter.file}#idea${idea}`;
+
+    const description =
+      chapter.order === docInfo.order
+        ? this.posInThisChapter(link, idea)
+        : this.posInAnotherChapter(link, idea, chapter.title);
+
+    return this.renderWrapper(description, [
+      {
+        click: this.resetPosition,
+        text: `👇 ${this.props.t('continue')}`,
+      },
+      {
+        link: `./${chapter.file}#idea${idea}`,
+        click: this.returnToPosition,
+        primary: true,
+        text: `🔙 ${this.props.t('return')}`,
+      },
+    ]);
+  };
+
+  posInThisChapter = (link: string, idea: number) => (
+    <Trans i18nKey="navigation:seqReturnThisChapter">
+      You read up to{' '}
+      <a href={link} onClick={this.highlightPosition}>
+        sentence #{{ idea }}
+      </a>{' '}
+      in this chapter.
+    </Trans>
+  );
+
+  posInAnotherChapter = (link: string, idea: number, title: string) => (
+    <Trans i18nKey="navigation:seqReturnAnotherChapter">
+      You read up to <a href={link}>sentence #{{ idea }}</a> in chapter <b>{{ title }}</b>.
+    </Trans>
+  );
 
   toggleCollapse = () => {
     this.setState({
@@ -125,40 +161,66 @@ class SeqReturn extends React.Component<IProps, IState> {
     });
   };
 
-  getCollapsedContent = () =>
-    this.props.status !== SeqReturnStatus.Initializing &&
-    this.props.targetChapter !== null &&
-    this.props.targetChapter.order !== null &&
-    this.props.targetIdea !== null
-      ? `🔙 ${this.props.targetChapter.order + 1}.${this.props.targetIdea}`
-      : '➕';
-
-  render() {
-    let getContent = null;
-    const classes = ['seq-return-wrapper'];
-
-    if (this.props.status === SeqReturnStatus.Disabled) return null;
-    if (this.props.status === SeqReturnStatus.Initializing) {
-      if (this.props.docRole === DocRole.Index) {
-        getContent = this.getOpenFirstChapterContent;
-        classes.push('seq-return-wrapper--high');
-      }
-    } else if (this.props.sequential === Sequential.No) {
-      getContent = this.getReturnToChapterContent;
-    }
+  renderWrapper(
+    content: JSX.Element | string | null,
+    buttons?: IButton[],
+    classes: string[] = [],
+    forceCollapsed?: boolean
+  ) {
+    const className = ['seq-return-wrapper'].concat(classes).join(' ');
 
     return (
-      getContent && (
-        <div className={classes.join(' ')}>
-          <div className={`seq-return ${this.state.collapsed ? 'seq-return--collapsed' : ''}`}>
+      <div className={className}>
+        <div className={`seq-return ${this.state.collapsed ? 'seq-return--collapsed' : ''}`}>
+          {docInfo.role !== DocRole.Index && (
             <div onClick={this.toggleCollapse} className="seq-return-toggle ui-target">
-              {this.state.collapsed ? this.getCollapsedContent() : '➖'}
+              {this.state.collapsed ? content : '➖'}
             </div>
-            {this.state.collapsed ? null : getContent()}
-          </div>
+          )}
+          {!this.state.collapsed && (
+            <>
+              {content && <p className="seq-text">{content}</p>}
+              {buttons && (
+                <div className="seq-buttons">
+                  {buttons.map((button: IButton, index: number) => (
+                    <a key={index} href={button.link || '#'} onClick={button.click}>
+                      {button.primary ? <b>{button.text}</b> : button.text}
+                    </a>
+                  ))}
+                </div>
+              )}
+            </>
+          )}
         </div>
-      )
+      </div>
     );
+  }
+
+  render() {
+    switch (this.props.status) {
+      case SeqReturnStatus.Disabled:
+        return null;
+      case SeqReturnStatus.Initializing:
+        switch (this.props.docRole) {
+          case DocRole.Index:
+            if (this.props.targetChapter !== null) return this.renderFirstOpen();
+            else return this.renderReturnFromTitle();
+          default:
+            return null;
+        }
+      case SeqReturnStatus.Enabled:
+        switch (this.props.docRole) {
+          case DocRole.Index:
+            return this.renderReturnFromTitle();
+          case DocRole.Chapter:
+            if (this.props.sequential === Sequential.No) return this.renderReturnFromChapter();
+            else return null;
+          default:
+            return this.renderReturnFromOther();
+        }
+      default:
+        return null;
+    }
   }
 }
 
